2cb51a1c90c305e946cdbb5e00b99406ff82621d
Aimee Lin <aimeelin@Aimees-MacBook-Pro.local>
2020-01-22 11:45:11 +1300

    updated index.js

--- index.js
+++ index.js
@@ -37,5 +37,5 @@ function saveNewPost(request, response) {
 app.post('/posts', saveNewPost)
 
 // listen for connections on port 3000
-app.listen(3000)
+app.listen(process.env.PORT || 3000)
 console.log("Hi! I am listening at http://localhost:3000")
