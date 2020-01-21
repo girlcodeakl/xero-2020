cdc669e5b8059fab3f33db117fee02ac9a4acbd7
penelope2007 <penelope.taateo14@gmail.com>
2020-01-22 10:53:44 +1300

    Posting pictures
    inserting url to make images appear on feed page

--- index.js
+++ index.js
@@ -25,6 +25,7 @@ function saveNewPost(request, response) {
   console.log(request.body.message) // write it on the command prompt so we can see
   let post = {}
   post.message = request.body.message
+  post.Image = request.body.Image
   posts.push(post) // save it in our list
   response.send("thanks for your message. Press back to add another")
 }
